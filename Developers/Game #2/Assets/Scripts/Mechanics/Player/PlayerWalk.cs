a994d9e50705eb97d45c0ae00b58b6817b686e02
Esteban Garcia <estebangarcia2121@gmail.com>
2019-12-13 16:36:49 -0800

    Project Creation

---
+++ Developers/Game #2/Assets/Scripts/Mechanics/Player/PlayerWalk.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour
+{
+    /*
+     * YOU CAN REMOVE THIS MESSAGE ONCE UR DONE
+     * 
+     * Hey guys good luck with the script :)
+     * - Esteban
+     * 
+     * Remember if you guys need help make sure to message me on discord just ping me or DM me
+     * 
+     * A few tips:
+     * 
+     */
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
