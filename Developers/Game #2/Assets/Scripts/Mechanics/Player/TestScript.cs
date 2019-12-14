347d1fa3fff65e02c22c24e5cdc72b1c5507796d
Esteban Garcia <estebangarcia2121@gmail.com>
2019-12-14 09:59:05 -0800

    Removed the test line

--- Developers/Game #2/Assets/Scripts/Mechanics/Player/TestScript.cs
+++ Developers/Game #2/Assets/Scripts/Mechanics/Player/TestScript.cs
@@ -7,7 +7,6 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Aye");
     }
 
     // Update is called once per frame
