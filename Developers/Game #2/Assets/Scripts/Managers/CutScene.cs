db5c00122df2547e8eb942bee5431e6b413503bd
Esteban Garcia <estebangarcia2121@gmail.com>
2019-12-14 19:06:43 -0800

    Created a text file with my name in it

---
+++ Developers/Game #2/Assets/Scripts/Managers/CutScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(PlayableDirector))]
+public class CutScene : MonoBehaviour
+{
+    private PlayableDirector cutscene;
+
+    protected void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            CutSceneManager.instance.LoadCutScene(cutscene, OnCutSceneEnd);
+        }
+    }
+
+    protected virtual void OnCutSceneEnd()
+    {
+
+    }
+}
---
+++ Developers/Game #2/Assets/Scripts/Managers/Puzzles/PuzzleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public static PuzzleManager instance;
+
+    public List<Puzzle> puzzles;
+
+    private void Start()
+    {
+        if (instance == null) instance = this;
+
+        puzzles = new List<Puzzle>();
+    }
+
+    public void StartPuzzle(string puzzleName)
+    {
+
+    }
+}
