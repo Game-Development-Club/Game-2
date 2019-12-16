8d897e55271b86d6fa68d2d3d46bd3a747b08fb0
Esteban Garcia <estebangarcia2121@gmail.com>
2019-12-15 18:37:46 -0800

    Slight change to cutscene management

--- Developers/Game #2/Assets/Scripts/Managers/CutScenes/CutScene.cs
+++ Developers/Game #2/Assets/Scripts/Managers/CutScenes/CutScene.cs
@@ -20,7 +20,7 @@ public class CutScene : MonoBehaviour
         bc = GetComponent<BoxCollider2D>();
     }
 
-    protected void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
