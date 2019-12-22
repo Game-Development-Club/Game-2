084dd57206bb7dec04ef8083066062c1fd9a225c
SarcasticWaffles <jpdearing9007@gmail.com>
2019-12-21 17:50:16 -0800

    (trying to) create jump script

--- Developers/Game #2/Assets/Player_Controller.cs
+++ Developers/Game #2/Assets/Player_Controller.cs
@@ -6,28 +6,24 @@ public class Player_Controller : MonoBehaviour
 {
 
     public float speed;
-
     private Rigidbody2D rb;
-    
+
     // Start is called before the first frame update
     void Start()
     {
 
-        rb = GetComponent<Rigidbody2D> ();
+        rb = GetComponent<Rigidbody2D>();
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void FixedUpdate()
     {
 
-        var move = Input.GetAxis ("Horizontal");
+        var move = Input.GetAxis("Horizontal");
 
         rb.velocity = new Vector2(speed * move, rb.velocity.y);
     }
+
 }
+
