fc706c968adb00df075ff826f5011983b00c1b48
Esteban Garcia <estebangarcia2121@gmail.com>
2019-12-21 18:24:22 -0800

    Added jackson and davids PlayerJump script

---
+++ Developers/Game #2/Assets/Scripts/Player/Player_Jump.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Jump : MonoBehaviour
+{
+
+    public float jumpForce;
+    private float moveInput;
+    private Rigidbody2D rb;
+
+    private bool isGrounded;
+    public Transform feetpos;
+    public float checkRadius;
+    public LayerMask WhatIsGround;
+
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      
+    }
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetpos.position, checkRadius, WhatIsGround);
+
+        if (moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+
+    }
+}
